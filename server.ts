c0ae2e830e9828c285887e7c4eed9316aaffefae
100lvlmaster <navinkodag@gmail.com>
2022-02-16 13:47:11 +0530

    minor

--- server.ts
+++ server.ts
@@ -6,7 +6,6 @@ const port = 3000;
 const testFun = (req, res: Response) => {
   res.status(200).json({ message: "server is running" });
 };
-
 app.listen(port, () => {
   console.log(`Timezones by location application is running on port ${port}.`);
 });
